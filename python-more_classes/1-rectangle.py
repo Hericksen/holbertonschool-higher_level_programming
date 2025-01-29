9d730da1a300d7baf8565b1d3dcce97209c2ea4d
Hericksen <gwendal.martin35@hotmail.fr>
2025-01-29 11:34:52 +0100

    Correction for checker

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,32 +3,37 @@
 
 
 class Rectangle:
-    '''Instantiaton'''
+    """A class that defines a rectangle."""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """Initialize the rectangle with optional width and height."""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
+        """Retrieve the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the width of the rectangle with validation."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be => 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
+        """Retrieve the height of the rectangle."""
         return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
+        """Set the height of the rectangle with validation."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be => 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
