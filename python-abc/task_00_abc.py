cb868f6bc2ac4b2cf2ab553dedf171796db36f53
Hericksen <gwendal.martin35@hotmail.fr>
2025-02-05 13:50:12 +0100

    Correction

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+
+# Defining the abstract class
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+# Implementing the Dog subclass
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+# Implementing the Cat subclass
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
