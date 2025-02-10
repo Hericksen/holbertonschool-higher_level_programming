f2e62d01fe9f84996d2414ff5db9aba77d013d3e
Hericksen <gwendal.martin35@hotmail.fr>
2025-02-10 16:49:12 +0100

    Comments

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -3,15 +3,15 @@
 
 
 class Student:
-    '''Constructor'''
-
+    '''Class student'''
     def __int__(self, first_name, last_name, age):
+        '''constructior'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    '''Retrive dictonnary representation'''
 
     def to_json(self, attrs=None):
+        '''Retrieve dictonnary representation'''
         if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
             return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
         return self.__dict__
--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,12 +4,10 @@
 
 class Student:
     '''Constructor'''
-
     def __int__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
     '''Retrieve dict representation'''
-
     def to_json(self):
         return self.__dict__
