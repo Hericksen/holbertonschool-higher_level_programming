d127e510b4dd13eea7dd539bbf224075a1bfa794
Hericksen <gwendal.martin35@hotmail.fr>
2025-02-10 16:35:24 +0100

    Task 5 to 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's triangle up to level n."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Initialize with the first row
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]  # First element is always 1
+
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)  # Last element is always 1
+        triangle.append(new_row)
+
+    return triangle
---
+++ python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Student to JSON'''
+
+
+class Student:
+    '''Constructor'''
+
+    def __int__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    '''Retrieve dict representation'''
+
+    def to_json(self):
+        return self.__dict__
