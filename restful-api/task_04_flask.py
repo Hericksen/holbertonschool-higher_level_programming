4699e13b0a6bc47aa61f58f7aeefee4e8beea4f8
Hericksen <gwendal.martin35@hotmail.fr>
2025-02-19 11:59:09 +0100

    Task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,7 +2,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+users = {"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+         "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}}
 
 
 @app.route('/')
