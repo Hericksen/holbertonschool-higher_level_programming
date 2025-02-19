f5b14a97ffd9abc02775f580ca65a3be238f83f4
Hericksen <gwendal.martin35@hotmail.fr>
2025-02-19 12:17:45 +0100

    Task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -3,13 +3,16 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
+# In-memory users dictionary
+users = {
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+}
+
+
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
-
-
-# In-memory users dictionary
-users = {}
 
 
 @app.route('/data')
