36cb2981ff10d54fb44ff9068f418c1aa6291f6b
Hericksen <gwendal.martin35@hotmail.fr>
2025-03-06 10:14:15 +0100

    Correction task 5

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
--- Number by score
-SELECT score, COUNT(*) AS Number
+-- lists the number of records with the same score in the second_table
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
